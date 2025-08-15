9bcc1541111d0164de7916cc63dd40e75fb7173c
Mahalakshmi025 <pmahalakshmi025@gmail.com>
2025-08-15 19:51:51 +0530

    sets

--- frontend.sh
+++ frontend.sh
@@ -32,21 +32,31 @@ VALIDATE(){
 
 echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
+CHECK_ROOT
+
 dnf install nginx -y &>>$LOG_FILE
-VALIDATE $? "Installing nginx"
+VALIDATE $? "Installing Nginx"
 
 systemctl enable nginx &>>$LOG_FILE
-VALIDATE $? "Enabled naginx"
+VALIDATE $? "Enable Nginx"
 
 systemctl start nginx &>>$LOG_FILE
-VALIDATE $? "Started nginx"
+VALIDATE $? "Start Nginx"
 
-rm -rf /usr/share/nginx/html?*
+rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
 VALIDATE $? "Removing default website"
 
-curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip
-VALIDATE $? "Downloading frontend code"
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOG_FILE
+VALIDATE $? "Downloding frontend code"
 
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>$LOG_FILE
 VALIDATE $? "Extract frontend code"
+
+cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf
+VALIDATE $? "Copied expense conf"
+
+systemctl restart nginx &>>$LOG_FILE
+VALIDATE $? "Restarted Nginx"
+
+
